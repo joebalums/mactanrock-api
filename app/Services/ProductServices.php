5ed8763727b97ed50a064c011c6dd58ddd60a2a1
Ivan Krister <ivankrister.garcia@gmail.com>
2022-09-16 11:34:43 +0800

    inventory

--- app/Services/ProductServices.php
+++ app/Services/ProductServices.php
@@ -13,6 +13,7 @@ class ProductServices
         $request = request();
         $product = new Product();
         $product->name = $request->get('name');
+        $product->code = $request->get('code');
         $product->description = $request->get('description');
         $product->category_id = $request->get('category_id');
         $product->unit_measurement = $request->get('unit_measurement');
@@ -20,20 +21,6 @@ class ProductServices
         $product->price = $request->get('price');
         $product->branch_id = $request->get('branch_id');
         $product->save();
-
-        if($request->has('initial_value') && $request->get('initial_value') > 0){
-            $inventory = new InventoryServices();
-            $inventory->in($product,$request->get('initial_value'),[
-                'from' => 'from_branch_id',
-                'from_value' =>  $request->get('branch_id'),
-                'description' => "Initial Inventory",
-                'action' => InventoryActionType::Manual,
-                'price' => $request->get('price'),
-                'expired_at' => $request->get('expired_at')
-            ]);
-
-            $product->refresh();
-        }
 
         return $product;
     }
