9ecdaf043e2344fa01a361bd017ea86417661f00
Joel Baluma <baluma.joel91@gmail.com>
2024-05-07 14:33:28 +0800

    up

--- app/Http/Controllers/Managements/UserPasswordController.php
+++ app/Http/Controllers/Managements/UserPasswordController.php
@@ -12,12 +12,12 @@ class UserPasswordController extends Controller
     public function update(UserServices $userServices, int $id)
     {
         request()->validate([
-            'old_password' => ['required','string'],
+            'old_password' => ['required', 'string'],
             'password' => ['required', 'confirmed', Rules\Password::defaults()],
         ]);
 
         $user = request()->user();
-        $userServices->changePassword(request()->password,$user);
+        $userServices->changePassword(request()->old_password, $user);
 
         return response()->noContent();
     }
